67419cbc9f452e60ae7c74a32cf7dc1050d80c94
James Gough <jpgough@gmail.com>
2023-11-05 11:18:35 -0500

    Add solution for Elf

--- src/main/java/org/advent/day1/Elf.java
+++ src/main/java/org/advent/day1/Elf.java
@@ -1,2 +1,30 @@
-package org.advent.day1;public class Elf {
+package org.advent.day1;
+
+/**
+ * Elf that is assigned calories
+ */
+public class Elf implements Comparable<Elf> {
+
+    private int totalCalories = 0;
+
+    /**
+     * Calories carried by the Elf
+     * @return total calories
+     */
+    public int getTotalCalories() {
+        return totalCalories;
+    }
+
+    /**
+     * Adds calories to this elf for carrying
+     * @param calories to add to total count for this elf
+     */
+    public void addCalories(int calories) {
+        totalCalories += calories;
+    }
+
+    @Override
+    public int compareTo(Elf that) {
+        return that.getTotalCalories() - this.getTotalCalories();
+    }
 }
