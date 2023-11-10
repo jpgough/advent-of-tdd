956c3a6e44ed7f51a9517796cc3756195bfad745
rocketstack-matt <matt@rocketstack.co>
2023-11-10 15:41:38 +0000

    Refactor exercise-1

--- src/test/java/org/advent/day1/TestElfShould.java
+++ src/test/java/org/advent/day1/TestElfShould.java
@@ -1,5 +1,7 @@
 package org.advent.day1;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.*;
@@ -14,41 +16,26 @@ public class TestElfShould {
 
     @Test
     void have_1000_total_calories_after_adding_1000() {
-        Elf elf = new Elf();
-        elf.addCalories(1000);
-        assertThat(elf.getTotalCalories(), equalTo(1000));
+       fail("Not yet implemented");
     }
 
     @Test
     void have_total_calories_equal_to_all_added_calories() {
-        Elf elf = new Elf();
-        elf.addCalories(1000);
-        elf.addCalories(2000);
-        elf.addCalories(3000);
-        assertThat(elf.getTotalCalories(), equalTo(6000));
+        fail("Not yet implemented");
     }
 
     @Test
     void compare_return_positive_value_for_elf_with_more_total_calories() {
-        Elf bigElf = new Elf();
-        bigElf.addCalories(5000);
-        Elf smallElf = new Elf();
-        smallElf.addCalories(3000);
-        assertThat(bigElf.compareTo(smallElf), greaterThan(0));
+        fail("Not yet implemented");
     }
 
     @Test
     void compare_zero_for_equal_elf() {
-        Elf elf = new Elf();
-        elf.addCalories(5000);
-        assertThat(elf.compareTo(elf), equalTo(0));
+        fail("Not yet implemented");
     }
+
     @Test
     void compare_return_negative_value_for_elf_with_less_total_calories() {
-        Elf bigElf = new Elf();
-        bigElf.addCalories(5000);
-        Elf smallElf = new Elf();
-        smallElf.addCalories(3000);
-        assertThat(smallElf.compareTo(bigElf), lessThan(0));
+        fail("Not yet implemented");
     }
 }
