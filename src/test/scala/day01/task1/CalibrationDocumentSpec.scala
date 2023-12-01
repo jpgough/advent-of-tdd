b7d6d98eaaa47014d3b4232de5a38a1bfaaa5684
Moleem <imre.molnar.0225@gmail.com>
2023-12-01 22:12:01 +0100

    DAY-1 Refactor

--- src/test/scala/day01/task1/CalibrationDocumentSpec.scala
+++ src/test/scala/day01/task1/CalibrationDocumentSpec.scala
@@ -2,43 +2,42 @@ package day01.task1
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
 
 
-class CalibrationDocumentSpec extends AnyFlatSpec with Matchers {
+class CalibrationDocumentSpec extends AnyFlatSpec with Matchers with TableDrivenPropertyChecks {
 
   behavior of "CalibrationDocument"
 
-  it should "fail if the document content is not valid" in {
-    val doc = new CalibrationDocument("25\nxx\n")
-    an [IllegalStateException] should be thrownBy doc.summarize()
+  forAll(Table(
+    ("content",   "scenario"),
+    ("",          "empty content"),
+    ("xx",        "content without digits"),
+    ("xx\n25",    "content without digits in each line")
+  )) {
+    (content: String, scenario: String) =>
+      it should s"fail on $scenario" in {
+        val doc = new CalibrationDocument(content)
+        an[IllegalStateException] should be thrownBy doc.summarize()
+      }
   }
 
-  it should "find the value from first and last digits" in {
-    val doc = new CalibrationDocument("1abc2")
-    doc.summarize() shouldBe 12
-  }
-
-  it should "find the value from first and last digits nested in the line" in {
-    val doc = new CalibrationDocument("pqr3stu8vwx")
-    doc.summarize() shouldBe 38
-  }
-
-  it should "find the value from first and last digits only" in {
-    val doc = new CalibrationDocument("a1b2c3d4e5f")
-    doc.summarize() shouldBe 15
-  }
-
-  it should "find the value even from a single digit" in {
-    val doc = new CalibrationDocument("treb7uchet")
-    doc.summarize() shouldBe 77
-  }
-
-  it should "sum values correctly" in {
-    val doc = new CalibrationDocument("""1abc2
-                                        |pqr3stu8vwx
-                                        |a1b2c3d4e5f
-                                        |treb7uchet""".stripMargin)
-    doc.summarize() shouldBe 142
+  forAll(Table(
+    ("content",     "expected value", "scenario"),
+    ("1abc2",       12,               "from first and last digits"),
+    ("pqr3stu8vwx", 38,               "from first and last digits nested in the line"),
+    ("a1b2c3d4e5f", 15,               "from first and last digits only"),
+    ("treb7uchet",  77,               "even from a single digit"),
+    ("""1abc2
+        |pqr3stu8vwx
+        |a1b2c3d4e5f
+        |treb7uchet""".stripMargin, 142, "sum correctly"),
+  )) {
+    (content: String, expectedValue: Int, scenario: String) =>
+      it should s"find the value $scenario" in {
+        val doc = new CalibrationDocument(content)
+        doc.summarize() shouldBe expectedValue
+      }
   }
 
 }
--- src/test/scala/day01/task2/CalibrationDocumentSpec.scala
+++ src/test/scala/day01/task2/CalibrationDocumentSpec.scala
@@ -2,141 +2,64 @@ package day01.task2
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
 
 
-class CalibrationDocumentSpec extends AnyFlatSpec with Matchers {
+class CalibrationDocumentSpec extends AnyFlatSpec with Matchers with TableDrivenPropertyChecks {
 
   behavior of "CalibrationDocument"
 
-  it should "fail if the document content is not valid" in {
-    val doc = new CalibrationDocument("25\nxx\n")
-    an [IllegalStateException] should be thrownBy doc.summarize()
+  forAll(Table(
+    ("content", "scenario"),
+    ("", "empty content"),
+    ("xx", "content without digits"),
+    ("xx\n25", "content without digits in each line")
+  )) {
+    (content: String, scenario: String) =>
+      it should s"fail on $scenario" in {
+        val doc = new CalibrationDocument(content)
+        an[IllegalStateException] should be thrownBy doc.summarize()
+      }
   }
 
-  it should "find the value from first and last digits" in {
-    val doc = new CalibrationDocument("1abc2")
-    doc.summarize() shouldBe 12
-  }
-
-  it should "find the value from first and last digits nested in the line" in {
-    val doc = new CalibrationDocument("pqr3stu8vwx")
-    doc.summarize() shouldBe 38
-  }
-
-  it should "find the value from first and last digits only" in {
-    val doc = new CalibrationDocument("a1b2c3d4e5f")
-    doc.summarize() shouldBe 15
-  }
-
-  it should "find the value even from a single digit" in {
-    val doc = new CalibrationDocument("treb7uchet")
-    doc.summarize() shouldBe 77
-  }
-
-  it should "find the value even if the number 'one' is spelled out" in {
-    val doc = new CalibrationDocument("one1")
-    doc.summarize() shouldBe 11
-  }
-
-  it should "find the value even if the number 'two' is spelled out" in {
-    val doc = new CalibrationDocument("two2")
-    doc.summarize() shouldBe 22
-  }
-
-  it should "find the value even if the number 'three' is spelled out" in {
-    val doc = new CalibrationDocument("three3")
-    doc.summarize() shouldBe 33
-  }
-
-  it should "find the value even if the number 'four' is spelled out" in {
-    val doc = new CalibrationDocument("four4")
-    doc.summarize() shouldBe 44
-  }
-
-  it should "find the value even if the number 'five' is spelled out" in {
-    val doc = new CalibrationDocument("five5")
-    doc.summarize() shouldBe 55
-  }
-
-  it should "find the value even if the number 'six' is spelled out" in {
-    val doc = new CalibrationDocument("six6")
-    doc.summarize() shouldBe 66
-  }
-
-  it should "find the value even if the number 'seven' is spelled out" in {
-    val doc = new CalibrationDocument("seven7")
-    doc.summarize() shouldBe 77
-  }
-
-  it should "find the value even if the number 'eight' is spelled out" in {
-    val doc = new CalibrationDocument("eight8")
-    doc.summarize() shouldBe 88
-  }
-
-  it should "find the value even if the number 'nine' is spelled out" in {
-    val doc = new CalibrationDocument("nine9")
-    doc.summarize() shouldBe 99
-  }
-
-  it should "find the value even if 'one' and 'eight' are merged" in {
-    val doc = new CalibrationDocument("oneight1")
-    doc.summarize() shouldBe 11
-  }
-
-  it should "find the value even if 'two' and 'one' are merged" in {
-    val doc = new CalibrationDocument("twone2")
-    doc.summarize() shouldBe 22
-  }
-
-  it should "find the value even if 'three' and 'eight' are merged" in {
-    val doc = new CalibrationDocument("threeight3")
-    doc.summarize() shouldBe 33
-  }
-
-  it should "find the value even if 'five' and 'eight' are merged" in {
-    val doc = new CalibrationDocument("fiveight5")
-    doc.summarize() shouldBe 55
-  }
-
-  it should "find the value even if 'seven' and 'nine' are merged" in {
-    val doc = new CalibrationDocument("sevenine7")
-    doc.summarize() shouldBe 77
-  }
-
-  it should "find the value even if 'eight' and 'two' are merged" in {
-    val doc = new CalibrationDocument("eightwo8")
-    doc.summarize() shouldBe 88
-  }
-
-  it should "find the value even if 'eight' and 'three' are merged" in {
-    val doc = new CalibrationDocument("eighthree8")
-    doc.summarize() shouldBe 88
-  }
-
-  it should "find the value even if 'three', 'eight' and 'three' are merged" in {
-    val doc = new CalibrationDocument("threeighthree")
-    doc.summarize() shouldBe 33
-  }
-
-  it should "find the value even if 'eight', 'three' and 'eight' are merged" in {
-    val doc = new CalibrationDocument("eighthreeight")
-    doc.summarize() shouldBe 88
-  }
-
-  it should "find the value even if both ends are merged" in {
-    val doc = new CalibrationDocument("eighthreethreeight")
-    doc.summarize() shouldBe 88
-  }
-
-  it should "sum values correctly" in {
-    val doc = new CalibrationDocument("""two1nine
-                                        |eightwothree
-                                        |abcone2threexyz
-                                        |xtwone3four
-                                        |4nineeightseven2
-                                        |zoneight234
-                                        |7pqrstsixteen""".stripMargin)
-    doc.summarize() shouldBe 281
+  forAll(Table(
+    ("content",            "expected value", "scenario"),
+    ("1abc2",              12,               "from first and last digits"),
+    ("pqr3stu8vwx",        38,               "from first and last digits nested in the line"),
+    ("a1b2c3d4e5f",        15,               "from first and last digits only"),
+    ("treb7uchet",         77,               "even from a single digit"),
+    ("one1",               11,               "even if the number 'one' is spelled out"),
+    ("two2",               22,               "even if the number 'two' is spelled out"),
+    ("three3",             33,               "even if the number 'three' is spelled out"),
+    ("four4",              44,               "even if the number 'four' is spelled out"),
+    ("five5",              55,               "even if the number 'five' is spelled out"),
+    ("six6",               66,               "even if the number 'six' is spelled out"),
+    ("seven7",             77,               "even if the number 'seven' is spelled out"),
+    ("eight8",             88,               "even if the number 'eight' is spelled out"),
+    ("nine9",              99,               "even if the number 'nine' is spelled out"),
+    ("oneight1",           11,               "even if 'one' and 'eight' are merged"),
+    ("twone2",             22,               "even if 'two' and 'one' are merged"),
+    ("threeight3",         33,               "even if 'three' and 'eight' are merged"),
+    ("fiveight5",          55,               "even if 'five' and 'eight' are merged"),
+    ("sevenine7",          77,               "even if 'seven' and 'nine' are merged"),
+    ("eightwo8",           88,               "even if 'eight' and 'two' are merged"),
+    ("eighthree8",         88,               "even if 'eight' and 'three' are merged"),
+    ("threeighthree",      33,               "even if 'three', 'eight' and 'three' are merged"),
+    ("eighthreeight",      88,               "even if 'eight', 'three' and 'eight' are merged"),
+    ("eighthreethreeight", 88,               "even if both ends are merged"),
+    ("""two1nine
+       |eightwothree
+       |abcone2threexyz
+       |xtwone3four
+       |4nineeightseven2
+       |zoneight234
+       |7pqrstsixteen""".stripMargin,                281 , "sum correctly"),
+  )) {
+    (content: String, expectedValue: Int, scenario: String) =>
+      it should s"find the value $scenario" in {
+        val doc = new CalibrationDocument(content)
+        doc.summarize() shouldBe expectedValue
+      }
   }
 
 }
