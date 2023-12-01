cfdcc16a766b76c26ff83a4e3e78273aad89f589
mollyvokes <mollyrvokes@gmail.com>
2023-12-01 19:39:46 +0000

    added handling for numbers written as strings

--- src/main/java/ms/advent/dayone/CalibrationCode.java
+++ src/main/java/ms/advent/dayone/CalibrationCode.java
@@ -1,18 +1,44 @@
 package ms.advent.dayone;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static java.lang.Character.isDigit;
 
 public class CalibrationCode {
+    public static Map<String, String> numberStrings = new HashMap<>();
+
+    static
+    {
+        numberStrings.put("one", "1");
+        numberStrings.put("two", "2");
+        numberStrings.put("three", "3");
+        numberStrings.put("four", "4");
+        numberStrings.put("five", "5");
+        numberStrings.put("six", "6");
+        numberStrings.put("seven", "7");
+        numberStrings.put("eight", "8");
+        numberStrings.put("nine", "9");
+    }
 
     public String getFirstNum(String code) {
 
         int leftPointer = 0;
         String leftDigit = "";
+        StringBuilder leftSubString = new StringBuilder();
 
         while (leftDigit.isEmpty()) {
-            if (isDigit(code.charAt(leftPointer))) {
+            for (String numberString: numberStrings.keySet()) {
+                if (String.valueOf(leftSubString).contains(numberString)) {
+                    leftDigit = numberStrings.get(numberString);
+                    break;
+                }
+            }
+
+            if (leftDigit.isEmpty() && isDigit(code.charAt(leftPointer))) {
                 leftDigit = String.valueOf(code.charAt(leftPointer));
-            } else {
+            } else if (leftDigit.isEmpty()) {
+                leftSubString.append(code.charAt(leftPointer));
                 leftPointer++;
             }
         }
@@ -24,11 +50,20 @@ public class CalibrationCode {
 
         int rightPointer = code.length() - 1;
         String rightDigit = "";
+        String rightSubString = "";
 
         while (rightDigit.isEmpty()) {
-            if (isDigit(code.charAt(rightPointer))) {
+            for (String numberString: numberStrings.keySet()) {
+                if (String.valueOf(rightSubString).contains(numberString)) {
+                    rightDigit = numberStrings.get(numberString);
+                    break;
+                }
+            }
+
+            if (rightDigit.isEmpty() && isDigit(code.charAt(rightPointer))) {
                 rightDigit = String.valueOf(code.charAt(rightPointer));
-            } else {
+            } else if (rightDigit.isEmpty()) {
+                rightSubString = code.charAt(rightPointer) + rightSubString;
                 rightPointer--;
             }
         }
@@ -37,9 +72,12 @@ public class CalibrationCode {
     }
 
     public int getTwoDigitCode(String code) {
-        String firstDigit = this.getFirstNum(code);
-        String lastDigit = this.getLastNum(code);
+        System.out.println(code);
 
+        String firstDigit = this.getFirstNum(code);
+        System.out.println(firstDigit);
+        String lastDigit = this.getLastNum(code);
+        System.out.println(lastDigit);
         String twoDigit = firstDigit + lastDigit;
 
         return Integer.parseInt(twoDigit);
