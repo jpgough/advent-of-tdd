f808f1831cbdcaa02fead5c1ac78a6288119e389
gjaya26 <152427833+gjaya26@users.noreply.github.com>
2023-12-02 12:49:23 -0500

    Update Elf.java

--- src/main/java/org/advent/day1/Elf.java
+++ src/main/java/org/advent/day1/Elf.java
@@ -18,6 +18,7 @@ public class Elf implements Comparable<Elf> {
     @Override
     public int compareTo(Elf arg0) {
         // TODO Auto-generated method stub
+        test
         throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
     }
 
