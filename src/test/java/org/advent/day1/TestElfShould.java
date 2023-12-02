5b6b721c631ebd30ccb77d18d318950a720f9705
gjaya26 <152427833+gjaya26@users.noreply.github.com>
2023-12-02 13:02:13 -0500

    Update TestElfShould.java
    Updated Test Cases.

--- src/test/java/org/advent/day1/TestElfShould.java
+++ src/test/java/org/advent/day1/TestElfShould.java
@@ -16,21 +16,29 @@ public class TestElfShould {
 
     @Test
     void have_1000_total_calories_after_adding_1000() {
-       fail("Not yet implemented");
+       Elf elf = new Elf();
+        elf.totalCalories = 1000;
+        assertThat(elf.getTotalCalories(), equalTo(1000));
     }
 
     @Test
     void compare_return_positive_value_for_elf_with_more_total_calories() {
-        fail("Not yet implemented");
+        Elf elf = new Elf();
+        elf.totalCalories = 1000;
+        assertThat(elf.compareTo(elf),equalTo(1));
     }
 
     @Test
     void compare_zero_for_equal_elf() {
-        fail("Not yet implemented");
+        Elf elf = new Elf();
+        elf.totalCalories = 0;
+        assertThat(elf.compareTo(elf),equalTo(0));        
     }
 
     @Test
     void compare_return_negative_value_for_elf_with_less_total_calories() {
-        fail("Not yet implemented");
+        Elf elf = new Elf();
+        elf.totalCalories = -10;
+        assertThat(elf.compareTo(elf),equalTo(-1));
     }
 }
